525e917e144197f00e44e4524ae83cdb5a71dd9b
DBKynd <dave@dbkynd.com>
2024-07-31 13:41:24 -0600

    add checkCollision function to Game class

--- src/script.js
+++ src/script.js
@@ -66,6 +66,13 @@ class Game {
     this.coins.forEach((coin) => coin.update());
   }
 
+  checkCollision(circle1, circle2) {
+    const dx = circle1.x - circle2.x;
+    const dy = circle1.y - circle2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < circle1.radius + circle2.radius;
+  }
+
   startBackgroundMusic() {
     this.backgroundMusic.play();
   }
