9fc121cb632dd9a7c31efcc78d9bd6d096fde257
DBKynd <dave@dbkynd.com>
2024-08-07 16:38:44 -0600

    boosting and level management

--- src/assets/scripts/bosses/TemporalSerpent.js
+++ src/assets/scripts/bosses/TemporalSerpent.js
@@ -104,7 +104,9 @@ class TemporalSerpent {
     ctx.restore();
   }
 
-  update() {}
+  update() {
+    this.checkCollisions();
+  }
 
   getDirectionTowardsPlayer() {
     const dx = this.game.player.x - this.x;
@@ -114,6 +116,11 @@ class TemporalSerpent {
     } else {
       return dy > 0 ? 'down' : 'up';
     }
+  }
+
+  checkCollisions() {
+    // BOMD // TODO
+    // Temporarily make the serpent leave the screen
   }
 }
 
