39265cd23d0c21cb03dba1a4124b0701c975ba00
taskrok <taskrok@gmail.com>
2024-08-13 12:39:18 -0600

    hud introduction

--- src/assets/scripts/enemies/BasicEnemies.js
+++ src/assets/scripts/enemies/BasicEnemies.js
@@ -57,10 +57,17 @@ class Enemy {
     }
 
     this.checkCollisions();
+
+    // Arrow Indicator Logic
+    const offscreen = this.x < 0 || this.x > this.game.canvas.width || this.y < 0 || this.y > this.game.canvas.height;
+    if (offscreen && !this.arrowAdded) {
+      this.game.addArrowIndicator(this);
+      this.arrowAdded = true; // Prevent adding multiple arrows
+    }
   }
 
   getSpawnPosition() {
-    this.x = this.side === 'left' ? -this.width * 0.5 : this.game.canvas.width + this.width * 0.5;
+    this.x = this.side === 'left' ? -this.width * 0.5 - 100: this.game.canvas.width + this.width * 0.5 + 100;
     this.y = getRandomYwithMargin(this.game, this.verticalMargin);
   }
 
