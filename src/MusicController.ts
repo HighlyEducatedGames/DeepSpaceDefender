01238441baf15a2592207ceebe8dfd5746c6a9b7
DBKynd <db@dbkynd.com>
2024-08-28 00:45:59 -0600

    music and menu

--- src/MusicController.ts
+++ src/MusicController.ts
@@ -1,21 +1,21 @@
 export default class MusicController {
-  currentTrack = null;
+  game: Game;
+  currentTrack: HTMLAudioElement | null = null;
   musicVol = parseFloat(localStorage.getItem('music_vol') || '0.5');
   fxVol = parseFloat(localStorage.getItem('fx_vol') || '0.5');
-  tracks = {
-    background: document.getElementById('background_music'),
-    gameOver: document.getElementById('game_over_music'),
-  };
+  tracks: { [key: string]: HTMLAudioElement };
 
-  constructor(game) {
-    /** @type {import('./Game.js').default} */
+  constructor(game: Game) {
     this.game = game;
     this.setMusicVolume(this.musicVol);
     this.setFxVolume(this.fxVol);
+    this.tracks = {
+      background: this.game.getAudio('background_music'),
+      gameOver: this.game.getAudio('game_over_music'),
+    };
   }
 
-  setTrack(track) {
-    if (!(track instanceof Audio)) return;
+  setTrack(track: HTMLAudioElement) {
     if (this.currentTrack === track) return;
     this.stop();
     this.currentTrack = track;
@@ -25,7 +25,7 @@ export default class MusicController {
 
   play() {
     if (!this.currentTrack) return;
-    this.currentTrack.play();
+    this.currentTrack.play().catch(() => {});
   }
 
   pause() {
@@ -39,32 +39,27 @@ export default class MusicController {
     this.currentTrack.currentTime = 0;
   }
 
-  restart() {
-    this.stop();
-    this.play();
-  }
-
-  setMusicVolume(value) {
+  setMusicVolume(value: number) {
     if (!value) return;
     this.musicVol = value;
     localStorage.setItem('music_vol', this.musicVol.toString());
-    const elements = document.getElementById('audio-sources').children;
-    const filtered = Array.from(elements).filter((x) => x.getAttribute('id').includes('music'));
+    const elements = document.getElementById('audio-sources')?.children as HTMLCollection;
+    const filtered = Array.from(elements).filter((x) => x.getAttribute('id')?.includes('music')) as HTMLAudioElement[];
     filtered.forEach((audio) => (audio.volume = value));
   }
 
-  setFxVolume(value) {
+  setFxVolume(value: number) {
     if (!value) return;
     this.fxVol = value;
     localStorage.setItem('fx_vol', this.fxVol.toString());
-    const elements = document.getElementById('audio-sources').children;
-    const filtered = Array.from(elements).filter((x) => x.getAttribute('id').includes('sound'));
+    const elements = document.getElementById('audio-sources')?.children as HTMLCollection;
+    const filtered = Array.from(elements).filter((x) => x.getAttribute('id')?.includes('sound')) as HTMLAudioElement[];
     filtered.forEach((audio) => (audio.volume = value));
   }
 
   stopAllFx() {
-    const elements = document.getElementById('audio-sources').children;
-    const filtered = Array.from(elements).filter((x) => x.getAttribute('id').includes('sound'));
+    const elements = document.getElementById('audio-sources')?.children as HTMLCollection;
+    const filtered = Array.from(elements).filter((x) => x.getAttribute('id')?.includes('sound')) as HTMLAudioElement[];
     filtered.forEach((audio) => {
       audio.pause();
       audio.currentTime = 0;
